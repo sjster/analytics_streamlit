cf1acbb367fa9e2903c26a87b415715839688be4
Srijith Rajamohan <sjster@gmail.com>
2022-02-16 14:40:13 -0500

    Darts forecast added

--- test.py
+++ test.py
@@ -6,6 +6,8 @@ import pandas as pd
 #from pymc3 import traceplot
 import pymc3 as pm
 import arviz as az
+from darts import TimeSeries
+from darts.models import ExponentialSmoothing, NaiveDrift
 from stackapi import StackAPI
 import pandas as pd
 import plotly.express as px
@@ -13,19 +15,32 @@ import plotly.express as px
 RUN_STACKOVERFLOW = 0
 st.set_page_config(layout='wide')
 
-if __name__ == '__main__':
 
-    query = 'pytorch'
+def get_time_series_trend(df, columns, fig):
+    dart_df = TimeSeries.from_dataframe(df.reset_index(), columns[0], columns[1])
+    model = NaiveDrift()
+    model.fit(dart_df)
+    trend = model.predict(10)
+    fig2 = px.line(trend.pd_dataframe(),title='Forecast')
+    #fig2.update_traces(marker_color='green')
+    fig.add_trace(fig2['data'][0])
+    fig.data[1].line.color = 'green'
+    fig.data[1].line.width = 8
+    print(fig)
+    return(fig)
 
-    def process_df(questions, query, type):
-      print(" --------------------- %s %s -------------------"%(query, type))
-      df_original = pd.json_normalize(questions['items'])
-      df = df_original[['title','score','view_count','answer_count','is_answered','tags','question_id']]
-      corr_matrix = df[['score', 'view_count','answer_count']].corr()
-      #df_original[['creation_date','last_activity_date']].hist()
-      #df[['score','view_count','answer_count']].hist()
-      return(df, df_original[['creation_date','last_activity_date']], corr_matrix)
 
+def process_df(questions, query, type):
+  print(" --------------------- %s %s -------------------"%(query, type))
+  df_original = pd.json_normalize(questions['items'])
+  df = df_original[['title','score','view_count','answer_count','is_answered','tags','question_id']]
+  corr_matrix = df[['score', 'view_count','answer_count']].corr()
+  #df_original[['creation_date','last_activity_date']].hist()
+  #df[['score','view_count','answer_count']].hist()
+  return(df, df_original[['creation_date','last_activity_date']], corr_matrix)
+
+
+def run_analytics(query):
     if(RUN_STACKOVERFLOW == 1):
         SITE = StackAPI('stackoverflow')
         questions_votes = SITE.fetch('questions', min=20, tagged=query, sort='votes')
@@ -41,7 +56,8 @@ if __name__ == '__main__':
         questions['closed_date'] = questions['closed_date'].astype('datetime64[s]')
         data = questions[questions['score'] >= 0]
     else:
-        questions = pd.read_json('mlflow_questions.json')
+        filename = query + '_questions.json'
+        questions = pd.read_json(filename)
         questions['creation_date'] = questions['creation_date'] / 1000
         questions['creation_date'] = questions['creation_date'].astype('datetime64[s]')
         questions['last_activity_date'] = questions['last_activity_date'] / 1000
@@ -73,6 +89,7 @@ if __name__ == '__main__':
 
     # --------------- EDA and feature extraction --------------- #
     questions['creation_date_only'] = pd.to_datetime(questions['creation_date'].dt.date)
+    # resample by month
     questions_by_week = questions.set_index('creation_date_only').resample('M').count()['question_id']
     answers_and_views_by_week = questions.set_index('creation_date_only').resample('M').sum()[['view_count', 'answer_count']]
 
@@ -113,7 +130,7 @@ if __name__ == '__main__':
               trace_t = pm.sample(1000)
         st.success('Complete')
         summary = az.summary(trace_t)
-        st.metric('Mean estimate for score', summary['mean'])
+        st.metric('Mean estimate for score (ignoring the negative scores)', summary['mean'])
         st.write(summary)
 
     col5, col6 = st.columns([1,1])
@@ -126,11 +143,22 @@ if __name__ == '__main__':
 
     # ---------------------- Question timeseries ------------------ #
     st.subheader("Time evolution of questions, answers and views")
-
     col7, col8 = st.columns([1,1])
-    fig = px.bar(questions_by_week, title="Number of questions by month")
+    fig = px.bar(questions_by_week, title="Number of questions by month, green trend line indicates forecast for next 10 months")
+    fig = get_time_series_trend(questions_by_week, ('creation_date_only', 'question_id'), fig)
     with col7:
         st.plotly_chart(fig)
     fig = px.bar(answers_and_views_by_week, title="Number of answers and views by month")
     with col8:
         st.plotly_chart(fig)
+
+    # -------------------- Time series forecast based on the trend ------------ #
+
+
+    #dart_df.plot(label='Past questions')
+    #trend.plot(label='Trend forecast for questions')
+
+if __name__ == '__main__':
+
+    query = 'pytorch'
+    run_analytics(query)
