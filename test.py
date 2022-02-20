43c334422aab051dc0dcc64f8b18839b01c05568
Srijith Rajamohan <sjster@gmail.com>
2022-02-20 17:01:29 -0500

    Added selection menu, moved logic into main

--- test.py
+++ test.py
@@ -14,6 +14,7 @@ import pandas as pd
 import plotly.express as px
 
 RUN_STACKOVERFLOW = 0
+count = 0
 st.set_page_config(layout='wide')
 
 #nlp = spacy.load("en_core_web_sm")
@@ -50,7 +51,43 @@ def process_df(questions, query, type):
   return(df, df_original[['creation_date','last_activity_date']], corr_matrix)
 
 
-def run_analytics(query):
+def plot_tags(df):
+    flat_list = [item for sublist in df['tags'].values for item in sublist]
+    tags_bar = pd.Series(flat_list).value_counts(ascending=False)
+    fig = px.bar(tags_bar)
+    fig.update_layout(height=500, width=1200)
+    fig.update_yaxes(type="log")
+    st.plotly_chart(fig)
+
+
+def run_analytics():
+
+    print("Rerunning")
+    # -------------------- Time series forecast based on the trend ------------ #
+
+
+def Poisson_inference(col):
+    with pm.Model() as model:
+      mu = pm.Uniform('mu', lower=0, upper=1000)
+      y = pm.Poisson('y', mu=mu, observed=col)
+      trace_t = pm.sample(1000)
+
+    return(trace_t)
+
+
+if __name__ == '__main__':
+
+    st.title("Stackoverflow queries")
+    if(RUN_STACKOVERFLOW == 0):
+        st.subheader('Reading cached data...')
+    else:
+        st.subheader('Loading from Stackoverflow...')
+    query = 'pytorch'
+    res = st.radio("Choose a framework", ('mlflow','netflix-metaflow','pytorch','tensorflow'), key='framework_selector', on_change=run_analytics)
+    run_analytics()
+
+    query = st.session_state.framework_selector
+
     if(RUN_STACKOVERFLOW == 1):
         SITE = StackAPI('stackoverflow')
         questions_votes = SITE.fetch('questions', min=20, tagged=query, sort='votes')
@@ -59,29 +96,56 @@ def run_analytics(query):
         df_activity_query, df_activity_query_dates, corr_matrix = process_df(questions_activity, query, 'activity')
         df_activity_unans = df_activity_query[df_activity_query['is_answered'] == False]
         questions = pd.json_normalize(questions_activity['items'])
+        print(questions)
         questions.to_json(query + '_questions.json')
-        questions['creation_date'] = questions['creation_date'].astype('datetime64[s]')
-        questions['last_activity_date'] = questions['last_activity_date'].astype('datetime64[s]')
-        questions['last_edit_date'] = questions['last_edit_date'].astype('datetime64[s]')
-        questions['closed_date'] = questions['closed_date'].astype('datetime64[s]')
+        columns = questions.columns
+        try:
+            questions['creation_date'] = questions['creation_date'].astype('datetime64[s]')
+        except:
+            raise Exception('creation_date does not exist')
+        try:
+            questions['last_activity_date'] = questions['last_activity_date'].astype('datetime64[s]')
+        except:
+            raise Exception('last_activity_date does not exist')
+        try:
+            questions['last_edit_date'] = questions['last_edit_date'].astype('datetime64[s]')
+        except:
+            raise Exception('last_edit_date does not exist')
+        try:
+            questions['closed_date'] = questions['closed_date'].astype('datetime64[s]')
+        except:
+            print('closed_date does not exist')
         data = questions[questions['score'] >= 0]
     else:
         filename = query + '_questions.json'
         print("Reading file ",filename)
         questions = pd.read_json(filename)
-        questions['creation_date'] = questions['creation_date'] / 1000
-        questions['creation_date'] = questions['creation_date'].astype('datetime64[s]')
-        questions['last_activity_date'] = questions['last_activity_date'] / 1000
-        questions['last_activity_date'] = questions['last_activity_date'].astype('datetime64[s]')
-        questions['last_edit_date'] = questions['last_edit_date'] / 1000
-        questions['last_edit_date'] = questions['last_edit_date'].astype('datetime64[s]')
-        questions['closed_date'] = questions['closed_date'] / 1000
-        questions['closed_date'] = questions['closed_date'].astype('datetime64[s]')
+        print(questions)
+        print(questions.columns)
+        try:
+            questions['creation_date'] = questions['creation_date'] / 1
+            questions['creation_date'] = questions['creation_date'].astype('datetime64[s]')
+        except:
+            raise Exception('creation_date does not exist')
+        try:
+            questions['last_activity_date'] = questions['last_activity_date'] / 1
+            questions['last_activity_date'] = questions['last_activity_date'].astype('datetime64[s]')
+        except:
+            raise Exception('last_activity_date does not exits')
+        try:
+            questions['last_edit_date'] = questions['last_edit_date'] / 1
+            questions['last_edit_date'] = questions['last_edit_date'].astype('datetime64[s]')
+        except:
+            raise Exception('last_edit_date does not exist')
+        try:
+            questions['closed_date'] = questions['closed_date'] / 1
+            questions['closed_date'] = questions['closed_date'].astype('datetime64[s]')
+        except:
+            print('closed_date does not exist')
         data = questions[questions['score'] >= 0]
         corr_matrix = questions[['score', 'view_count','answer_count']].corr()
 
     # -------------- Header info ----------------#
-    st.title("Stackoverflow queries for " + query)
     st.subheader("Data")
     headercol1, headercol2, headercol3, headercol4 = st.columns([1,1,1,1])
     num_questions = len(questions)
@@ -92,10 +156,10 @@ def run_analytics(query):
     headercol4.metric("Questions with negative score", len(questions) - len(data))
     st.write(questions, width=20, height=100)
 
-    age = st.slider('Top n terms', 0, 130, 25)
+    #age = st.slider('Top n terms', key='slider', 0, 130, 25)
 
     st.subheader("Summarize the data")
-    if st.checkbox('Show dataframe'):
+    if st.checkbox(label='Show dataframe', key='dataframe_show'):
         st.write(questions.describe(include='all'))
     st.subheader("Correlation matrix")
     st.write(corr_matrix)
@@ -103,12 +167,7 @@ def run_analytics(query):
     # ---------------- Tags --------------- #
     st.subheader("Distribution of tags in questions")
     #questions['entities'] = questions['title'].apply(lambda x: apply_spacy(x))
-    flat_list = [item for sublist in questions['tags'].values for item in sublist]
-    tags_bar = pd.Series(flat_list).value_counts(ascending=False)
-    fig = px.bar(tags_bar)
-    fig.update_layout(height=500, width=1200)
-    fig.update_yaxes(type="log")
-    st.plotly_chart(fig)
+    plot_tags(questions)
 
     # --------------- EDA and feature extraction --------------- #
     questions['creation_date_only'] = pd.to_datetime(questions['creation_date'].dt.date)
@@ -128,14 +187,11 @@ def run_analytics(query):
     with col2:
         st.text('Bayesian inference of the parameters of the views distribution \n\n')
         with st.spinner('Inferring the parameters of the view distribution (Poisson)'):
-            with pm.Model() as model:
-              mu = pm.Uniform('mu', lower=0, upper=1000)
-              y = pm.Poisson('y', mu=mu, observed=questions['view_count'])
-              trace_t = pm.sample(1000)
-        st.success('Complete')
-        summary = az.summary(trace_t)
-        st.metric('Mean estimate for views', summary['mean'])
-        st.write(summary)
+            trace_t = Poisson_inference(questions['view_count'])
+            st.success('Complete')
+            summary = az.summary(trace_t)
+            st.metric('Mean estimate for views', summary['mean'])
+            st.write(summary)
 
     # -------------------- Scores --------------- #
     st.subheader("Scores")
@@ -167,6 +223,7 @@ def run_analytics(query):
     # ---------------------- Question timeseries ------------------ #
     st.subheader("Time evolution of questions, answers and views")
     col7, col8 = st.columns([1,1])
+    print(questions_by_week)
     fig = px.bar(questions_by_week, title="Number of questions by month, green trend line indicates forecast for next 10 months")
     fig = get_time_series_trend(questions_by_week, ('creation_date_only', 'question_id'), fig)
     with col7:
@@ -174,12 +231,3 @@ def run_analytics(query):
     fig = px.bar(answers_and_views_by_week, title="Number of answers and views by month")
     with col8:
         st.plotly_chart(fig)
-
-    # -------------------- Time series forecast based on the trend ------------ #
-
-
-
-if __name__ == '__main__':
-
-    query = 'mlflow'
-    run_analytics(query)
